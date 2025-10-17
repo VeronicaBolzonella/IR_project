5528789da13012d24c2a8bb57634d189bc32c386
Veronica <veronica.bolzonella@gmail.com>
2025-10-17 14:50:18 +0200

    class reranker

--- models/rerankmodel.py
+++ models/rerankmodel.py
@@ -1,6 +1,42 @@
 # Get the queries
 import json
 
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+
+from pyserini.search.lucene import LuceneSearcher
+
+class Reranker():
+    def __init__(self, searcher):
+        self.encoder = AutoModelForSequenceClassification.from_pretrained('cross-encoder/ms-marco-MiniLM-L-6-v2')
+        self.tokenizer = AutoTokenizer.from_pretrained('cross-encoder/ms-marco-MiniLM-L-6-v2')
+
+    def rank(self, index, queries):
+        self.encoder.eval()
+        searcher = LuceneSearcher(index)
+        results = {}
+
+        for qid, q in queries.items():
+            # First Pass BM25 (Lucene)
+            hits = searcher.search(q, k=1000)
+        
+            # Scores per query and document id
+            docs = []
+            for hit in hits:
+                doc = searcher.doc(hit.docid).raw()
+                docs.append(doc)
+        
+            features = self.tokenizer(q, docs, padding=True, truncation=True, return_tensors='pt')
+            with torch.no_grad():
+                score = self.encoder(**features).logits
+            results[qid] = score
+                    
+
+        
+model = Reranker()
+
+
+
 queries = {}
 
 with open('data/longfact-objects_celebrities.jsonl', 'r', encoding='utf-8') as f:
@@ -12,7 +48,6 @@ with open('data/longfact-objects_celebrities.jsonl', 'r', encoding='utf-8') as f
 
 
 # Lucene Searcher
-from pyserini.search.lucene import LuceneSearcher
 
 searcher = LuceneSearcher('indexes/wiki_dump_index')
 
@@ -29,8 +64,6 @@ for qid, q in queries.items():
         #print(res)
         
 # Cross Encoder
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-import torch
 
 model = AutoModelForSequenceClassification.from_pretrained('cross-encoder/ms-marco-MiniLM-L-6-v2')
 tokenizer = AutoTokenizer.from_pretrained('cross-encoder/ms-marco-MiniLM-L-6-v2')
