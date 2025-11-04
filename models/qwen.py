346170d5c07bc36dfd75c69e61287fd3fceba0db
Veronica <veronica.bolzonella@gmail.com>
2025-11-04 17:45:40 +0100

    added example from qwen

---
+++ models/qwen.py
@@ -0,0 +1,38 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+class Qwen():
+    def __init__(self, model_name:str="Qwen/Qwen2.5-7B-Instruct"):
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_name,
+            torch_dtype="auto",
+            device_map="auto"
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    def prompt(self, prompt:str):
+        # up to 32,768 tokens
+        messages = [
+            {"role": "system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."},
+            {"role": "user", "content": prompt}
+        ]
+        text = self.tokenizer.apply_chat_template(
+            messages,
+            tokenize=False,
+            add_generation_prompt=True
+        )
+
+        model_inputs = self.tokenizer([text], return_tensors="pt").to(self.device)
+
+        generated_ids = self.model.generate(
+            **model_inputs,
+            max_new_tokens=512
+        )
+        generated_ids = [
+            output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+        ]
+
+        response = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+        return response
