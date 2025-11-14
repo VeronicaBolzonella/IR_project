36c29adbfd01d346309ca3f8c1e1e0ccc10c1959
Ana Leopold <ana.leopold@ru.nl>
2025-11-14 11:59:11 +0100

    Qwen with API (gives error for the API key)

--- models/qwen.py
+++ models/qwen.py
@@ -1,38 +1,69 @@
-import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
+# import torch
+# from transformers import AutoModelForCausalLM, AutoTokenizer
 
-class Qwen():
-    def __init__(self, model_name:str="Qwen/Qwen2.5-7B-Instruct"):
-        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        self.model = AutoModelForCausalLM.from_pretrained(
-            model_name,
-            torch_dtype="auto",
-            device_map="auto"
-        )
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+# class Qwen():
+#     def __init__(self, model_name:str="Qwen/Qwen2.5-7B-Instruct"):
+#         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+#         self.model = AutoModelForCausalLM.from_pretrained(
+#             model_name,
+#             torch_dtype="auto",
+#             device_map="auto"
+#         )
+#         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    def prompt(self, prompt:str):
-        # up to 32,768 tokens
-        messages = [
-            {"role": "system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."},
-            {"role": "user", "content": prompt}
-        ]
-        text = self.tokenizer.apply_chat_template(
-            messages,
-            tokenize=False,
-            add_generation_prompt=True
-        )
+#     def prompt(self, prompt:str):
+#         # up to 32,768 tokens
+#         messages = [
+#             {"role": "system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."},
+#             {"role": "user", "content": prompt}
+#         ]
+#         text = self.tokenizer.apply_chat_template(
+#             messages,
+#             tokenize=False,
+#             add_generation_prompt=True
+#         )
 
-        model_inputs = self.tokenizer([text], return_tensors="pt").to(self.device)
+#         model_inputs = self.tokenizer([text], return_tensors="pt").to(self.device)
 
-        generated_ids = self.model.generate(
-            **model_inputs,
-            max_new_tokens=512
-        )
-        generated_ids = [
-            output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
-        ]
+#         generated_ids = self.model.generate(
+#             **model_inputs,
+#             max_new_tokens=512
+#         )
+#         generated_ids = [
+#             output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+#         ]
 
-        response = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+#         response = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
 
-        return response
+#         return response
+
+
+# Code from https://modelstudio.console.alibabacloud.com/?tab=doc#/doc/?type=model&url=2840915
+# API_KEY => the environment variable in the terminal like this: export API_KEY="your_api_key_here"
+
+
+
+import os
+from openai import OpenAI
+
+try:
+    client = OpenAI(
+        # The API keys for the Singapore and China (Beijing) regions are different. To obtain an API key, see https://modelstudio.console.alibabacloud.com/?tab=model#/api-key
+        # If you have not configured an environment variable, replace the following line with your Model Studio API key: api_key="sk-xxx",
+        api_key=os.getenv("API_KEY"),
+        # The following URL is for the Singapore region. If you use a model in the China (Beijing) region, replace the URL with: https://dashscope.aliyuncs.com/compatible-mode/v1
+        base_url="https://dashscope-intl.aliyuncs.com/compatible-mode/v1",
+    )
+
+    completion = client.chat.completions.create(
+        model="Qwen2.5-7B",  
+        messages=[
+            {'role': 'system', 'content': 'You are a helpful assistant.'},
+            {'role': 'user', 'content': 'Who are you?'}
+            ]
+    )
+    print(completion.choices[0].message.content)
+except Exception as e:
+    print(f"Error message: {e}")
+    print("For more information, see https://www.alibabacloud.com/help/en/model-studio/developer-reference/error-code")
+
