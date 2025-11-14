d407ef1be1c060792971a0d841ce0ac04b66e4e6
Ana Leopold <ana.leopold@ru.nl>
2025-11-14 13:47:56 +0100

    Add model_kwargs

--- evaluation/ue.py
+++ evaluation/ue.py
@@ -4,7 +4,7 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from models.qwen import Qwen
 
-def generate_with_ue(prompt, api=True):
+def generate_with_ue(prompt, api=False):
     tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-0.5B-Instruct")
     model = AutoModelForCausalLM.from_pretrained(
         "Qwen/Qwen2.5-0.5B-Instruct",
@@ -15,6 +15,7 @@ def generate_with_ue(prompt, api=True):
     # This is required for TruthTorch
     model.config.output_hidden_states = True
     model.config.output_attentions = True
+    model.config.use_cache = False
     model.config.return_dict = True
     
     
@@ -44,7 +45,13 @@ def generate_with_ue(prompt, api=True):
             messages=messages,
             truth_methods=truth_methods,
             max_new_tokens=100,
-            temperature=0.7
+            temperature=0.7,
+            model_kwargs={
+                "output_attentions": True,
+                "output_hidden_states": True,
+                "use_cache": False,
+                "return_dict": True
+            }   
         )
     return output
 
