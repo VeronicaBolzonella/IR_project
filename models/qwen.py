a54a860f7ec8f4a98e5c2abf1e6efe9f8a80fcad
Ana Leopold <ana.leopold@ru.nl>
2025-11-14 12:51:43 +0100

    Two qwen options with and without API

--- models/qwen.py
+++ models/qwen.py
@@ -1,47 +1,51 @@
-# import torch
-# from transformers import AutoModelForCausalLM, AutoTokenizer
+# QWEN LOCAL (works on the cluster!)
 
-# class Qwen():
-#     def __init__(self, model_name:str="Qwen/Qwen2.5-7B-Instruct"):
-#         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-#         self.model = AutoModelForCausalLM.from_pretrained(
-#             model_name,
-#             torch_dtype="auto",
-#             device_map="auto"
-#         )
-#         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-#     def prompt(self, prompt:str):
-#         # up to 32,768 tokens
-#         messages = [
-#             {"role": "system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."},
-#             {"role": "user", "content": prompt}
-#         ]
-#         text = self.tokenizer.apply_chat_template(
-#             messages,
-#             tokenize=False,
-#             add_generation_prompt=True
-#         )
+class Qwen():
+    def __init__(self, model_name:str="Qwen/Qwen2.5-7B-Instruct"):
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_name,
+            torch_dtype="auto",
+            device_map="auto"
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-#         model_inputs = self.tokenizer([text], return_tensors="pt").to(self.device)
+    def prompt(self, prompt:str):
+        # up to 32,768 tokens
+        messages = [
+            {"role": "system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."},
+            {"role": "user", "content": prompt}
+        ]
+        text = self.tokenizer.apply_chat_template(
+            messages,
+            tokenize=False,
+            add_generation_prompt=True
+        )
 
-#         generated_ids = self.model.generate(
-#             **model_inputs,
-#             max_new_tokens=512
-#         )
-#         generated_ids = [
-#             output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
-#         ]
+        model_inputs = self.tokenizer([text], return_tensors="pt").to(self.device)
 
-#         response = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        generated_ids = self.model.generate(
+            **model_inputs,
+            max_new_tokens=512
+        )
+        generated_ids = [
+            output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+        ]
 
-#         return response
+        response = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+        return response
 
 
+
+
+
+# QWEN API VERSION - DOES NOT WORK (GIVES ERROR FOR API_KEY)
 # Code from https://modelstudio.console.alibabacloud.com/?tab=doc#/doc/?type=model&url=2840915
 # API_KEY => the environment variable in the terminal like this: export API_KEY="your_api_key_here"
-
-
 
 import os
 from openai import OpenAI
@@ -56,7 +60,7 @@ try:
     )
 
     completion = client.chat.completions.create(
-        model="Qwen2.5-7B",  
+        model="qwen2.5-7b-instruct",  
         messages=[
             {'role': 'system', 'content': 'You are a helpful assistant.'},
             {'role': 'user', 'content': 'Who are you?'}
