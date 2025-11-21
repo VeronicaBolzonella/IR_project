361b240ff103c57532e919a8d2068eba4271abca
Veronica <veronica.bolzonella@gmail.com>
2025-11-21 15:36:21 +0100

    script test

--- scripts/job.sh
+++ scripts/job.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-#SBATCH --partition=csedu
-#SBATCH --account=csedui00041
-#SBATCH --gres=gpu:1
-#SBATCH --mem=16G
-#SBATCH --output=output.out
+
 
 source /vol/csedu-nobackup/course/I00041_informationretrieval/users/vbolzonella/IR_project/.venv/bin/activate
 
