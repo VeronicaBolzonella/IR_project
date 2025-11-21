bb8aa0504fcf171d3aa58f53aad73d55cabef6e7
Veronica <veronica.bolzonella@gmail.com>
2025-11-21 16:03:13 +0100

    added ues

---
+++ evaluation/ue.py
@@ -0,0 +1,55 @@
+import TruthTorchLM as ttlm
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from models.qwen import Qwen
+
+def generate_with_ue(prompt, model, api=False):
+    tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-0.5B-Instruct")
+    model = AutoModelForCausalLM.from_pretrained(
+        "Qwen/Qwen2.5-0.5B-Instruct",
+        device_map="auto",
+        torch_dtype=torch.float16
+    )
+    
+    # tokenizer = model.tokenizer
+    # model = model.model
+    
+    
+    sum_of_eigen = ttlm.truth_methods.SumEigenUncertainty()
+    semantic_entropy = ttlm.truth_methods.SemanticEntropy()
+    
+    truth_methods = [sum_of_eigen, semantic_entropy]
+
+    messages = [
+                {"role": "system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."},
+                {"role": "user", "content": prompt}
+            ]
+    #api = False
+    #model = "distilgpt2"  
+
+    if api:
+        # Generate text with truth values (api model)
+        output = ttlm.generate_with_truth_value(
+            model=model,
+            messages=messages,
+            truth_methods=truth_methods
+        )
+    else:
+        output = ttlm.generate_with_truth_value(
+            model=model,
+            tokenizer=tokenizer,
+            messages=messages,
+            truth_methods=truth_methods,
+            max_new_tokens=100,
+            temperature=0.7,   
+        )
+    return output
+
+
+#qwen_model = Qwen()
+#ue_values = generate_with_ue("What is the capital of France?", model = qwen_model, api=False)
+
+ue_values = generate_with_ue("What is the capital of France?", model=None, api=False) # none means its defined inside the function
+print(ue_values['normalized_truth_values'])
+
